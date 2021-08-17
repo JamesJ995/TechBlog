2b80fdb78b2652f92d207da314639eec72745cc9
jamesj995 <mrjam7@gmail.com>
2021-08-16 21:55:34 -0500

    login, profile, post working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -41,8 +41,28 @@ router.get('/posts/:id', async (req, res) => {
     const post = postData.get({ plain: true });
 
     res.render('post', {
-      post,
+      ...post,
       logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
