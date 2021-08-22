1f478cf109559f70bd814d6bd9c52a83d571a806
jamesj995 <mrjam7@gmail.com>
2021-08-22 16:22:15 -0500

    seeded comments are working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,7 +34,7 @@ router.get('/posts/:id', async (req, res) => {
     });
 
     const post = postData.get({ plain: true });
-    console.log(post);
+
     res.render('post', {
       ...post,
       logged_in: req.session.logged_in,
